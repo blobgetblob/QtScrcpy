241174724297fc08e7f5af6469f78bfc81cb0edc
Dwi Susanto <wis4nto@gmail.com>
2024-07-26 20:48:52 +0700

    Hide again tool form

--- QtScrcpy/ui/videoform.cpp
+++ QtScrcpy/ui/videoform.cpp
@@ -164,13 +164,13 @@ void VideoForm::setSerial(const QString &serial)
 
 void VideoForm::showToolForm(bool show)
 {
-    //(void)show;
-    if (!m_toolForm) {
-        m_toolForm = new ToolForm(this, ToolForm::AP_OUTSIDE_RIGHT);
-        m_toolForm->setSerial(m_serial);
-    }
-    m_toolForm->move(pos().x() + geometry().width(), pos().y() + 30);
-    m_toolForm->setVisible(show);
+    (void)show;
+    // if (!m_toolForm) {
+    //     m_toolForm = new ToolForm(this, ToolForm::AP_OUTSIDE_RIGHT);
+    //     m_toolForm->setSerial(m_serial);
+    // }
+    // m_toolForm->move(pos().x() + geometry().width(), pos().y() + 30);
+    // m_toolForm->setVisible(show);
 }
 
 void VideoForm::moveCenter()
